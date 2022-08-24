ebed765dd185c5c18d06f3ad6c73fdebe4f9a5f4
xxx480 <37895103+xxx480@users.noreply.github.com>
2022-08-24 20:12:53 +0300

    format Nim code

--- Nim/main.nim
+++ Nim/main.nim
@@ -1,20 +1,22 @@
 import std/random
 
- 
-proc calculate_pi(n: int): float = 
+
+proc calculate_pi(n: int): float =
   randomize()
 
   var count: int = 0
   for i in 0..n:
     var x = rand(1.0)
     var y = rand(1.0)
-    if x*x + y*y < 1:
+    if x * x + y * y < 1:
       count += 1
 
-  return 4.0*float(count)/float(n)
+  return 4.0 * float(count) / float(n)
+
 
 proc main() =
   var number_of_itteration: int = 1_000_000
   echo calculate_pi(number_of_itteration)
+
 
 main()
